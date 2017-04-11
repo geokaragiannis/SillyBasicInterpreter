b96304351c0780be9eff082b9f3e41398df46f9b
Marko J Jerkovic <mjerkovi@unix2.lt.ucsc.edu>
2017-04-10 18:38:46 -0700

    Labels are now being added to the label hash table

--- sbi.scm
+++ sbi.scm
@@ -12,6 +12,32 @@
 ;;    program, which is the executed.  Currently it is only printed.
 ;;
 
+;; *label-table* is hash table that will store all of the labels
+(define *label-table* (make-hash))
+(define (label-get key)
+        (hash-ref *label-table* key))
+(define (label-put! key value)
+        (hash-set! *label-table* key value))
+
+;; *function-table* is hash table that will store all of the functions
+(define *function-table* (make-hash))
+(define (function-get key)
+        (hash-ref *function-table* key))
+(define (function-put! key value)
+        (hash-set! *function-table* key value))
+
+
+;; *variable-table* is hash table that will store all of the variables
+(define *variable-table* (make-hash))
+(define (variable-get key)
+        (hash-ref *variable-table* key))
+(define (variable-put! key value)
+        (hash-set! *variable-table* key value))
+
+(variable-put! 'pi 3.141592653589793238462643383279502884197169399)
+(variable-put! 'e 2.718281828459045235360287471352662497757247093)
+
+
 (define *stderr* (current-error-port))
 
 (define *run-file*
@@ -44,14 +70,44 @@
     (printf "~a: ~s~n" *run-file* filename)
     (printf "==================================================~n")
     (printf "(~n")
-    (map (lambda (line) (printf "~s~n" line)) program)
+    (map (lambda (line) (printf "~s~n" line)) program) 
     (printf ")~n"))
+
+(define (add-line-to-label line) 
+    ;; first checks to see if the cdr is null, in which case there is no
+    ;; label so return 0. otherwise, check to see if the cadr is a symbol,
+    ;; if so, the symbol is the label so add it to the hash table. otherwise
+    ;; return 0
+    (if (null? (cdr line))
+        0
+        [ if (symbol? (cadr line))
+              (label-put! (cadr line) (car line))
+              0]
+    ) 
+)
 
 (define (main arglist)
     (if (or (null? arglist) (not (null? (cdr arglist))))
         (usage-exit)
+        ;; else statement
         (let* ((sbprogfile (car arglist))
-               (program (readlist-from-inputfile sbprogfile)))
-              (write-program-by-line sbprogfile program))))
+               (program (readlist-from-inputfile sbprogfile))) 
+               (map (lambda(line) (add-line-to-label line)) program)           
+               ;printing labels
+               (hash-for-each *label-table*
+                    (lambda (key value)
+                        (printf "~s : ~s ~n" key value))
+               )
+               (newline)
+               ;printing variables
+               (hash-for-each *variable-table*
+                    (lambda (key value)
+                        (printf "~s : ~s ~n" key value))
+               )
+
+                
+
+             ; (write-program-by-line sbprogfile program)
+)))
 
 (main (vector->list (current-command-line-arguments)))
